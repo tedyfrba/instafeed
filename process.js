89fdf6a2668ae17b285d8979a29219a6d7f100c0
tadeo.granese <tgranese@frba.utn.edu.ar>
2021-08-04 17:19:44 +0100

    improve into callback errback and format json output

--- process.js
+++ process.js
@@ -2,8 +2,32 @@ const fs = require('fs');
 const vldtn = require('./lib/validation');
 
 // leer article.json
+function jsonReader(filePath, callback) {
+    fs.readFile(filePath, 'utf8' , (err, data) => {
+        if (err) {
+            return callback && callback(err)
+        }
+        try {
+            const object = JSON.parse(data)
+            return callback && callback(null, object)
+        } catch(err) {
+            return callback && callback(err)
+        }
+    })
+}
 
-fs.readFile('./article.json', 'utf8' , (err, data) => {
-  if (err) throw err;
-  vldtn.validateWithYup(JSON.parse(data));
+jsonReader('./article.json', (err, data) => {
+    if (err) {
+        console.error(err);
+        return
+    }
+
+    vldtn.validateWithYup(data, (err, data) => {
+      if (err) {
+        console.error(err);
+      }
+      console.log(data);
+
+    });
+
 })
