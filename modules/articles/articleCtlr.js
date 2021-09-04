3471176149b1d37167fca11b0517094a52a2112c
tadeo.granese <tgranese@frba.utn.edu.ar>
2021-09-04 15:15:23 +0100

    Completed rest of Article CRUD

--- modules/articles/articleCtlr.js
+++ modules/articles/articleCtlr.js
@@ -1,4 +1,5 @@
 var Article = require('./articleMdl');
+const vldtn = require('../../lib/validation');
 
 // Display list of all Articles.
 exports.list = async function(req, res, next) {
@@ -11,15 +12,18 @@ exports.list = async function(req, res, next) {
 exports.detail = async function(req, res, next) {
     Article.findById(req.params.id).exec()
       .then(data => {
-        res.json(data)
-    })
+            if (!data)
+                res.status(404).json()
+            else
+                res.json(data)
+        })
       .catch(err => next(err));
 };
 
 // Handle Article create on POST.
 exports.create_post = [
     (req, res, next) => {
-        // Create a Article object with escaped and trimmed data.
+        // Create a Article object.
         var article = new Article(
           { _id: req.body.id,
             title: req.body.title,
@@ -37,3 +41,55 @@ exports.create_post = [
             .catch(err => next(err));
     }
 ];
+
+// Handle Article update on PUT.
+exports.update_put = [
+
+    (req, res, next) => {
+        // Create a Article object.
+        var article = new Article(
+          { _id: req.body.id,
+            title: req.body.title,
+            url: req.body.url,
+            keywords: req.body.keywords,
+            modifiedAt: req.body.modifiedAt,
+            publishedAt: req.body.publishedAt,
+            author: req.body.author,
+            readMins: req.body.readMins,
+            source: req.body.source
+           });
+
+        Article.findByIdAndUpdate(req.params.id, article)
+            .then(data => res.json(data))
+            .catch(err => next(err));
+    }
+];
+
+// Handle Article update on PATCH.
+exports.update_patch = [
+
+    (req, res, next) => {
+        
+        var updateOps = {};
+        for (let [key, value] of Object.entries(req.body)) {
+          updateOps[key] = value;
+        }
+
+        Article.findByIdAndUpdate(req.params.id, updateOps)
+            .then(data => res.json(data))
+            .catch(err => next(err));
+    }
+];
+
+// Handle Article on DELETE.
+exports.delete = async function(req, res, next) {
+    // Assume the post has valid id (ie no validation/sanitization).
+    Article.findByIdAndRemove(req.params.id).exec()
+          .then(data => {
+                if (!data)
+                    res.status(404).json()
+                else
+                    res.status(204).json(data)
+            })
+          .catch(err => next(err));
+};
--- modules/articles/articlesRtr.js
+++ modules/articles/articlesRtr.js
@@ -6,5 +6,8 @@ let router = express.Router()
 router.get('/', articleCtlr.list);
 router.get('/:id', articleCtlr.detail);
 router.post('/', articleCtlr.create_post);
+router.put('/:id', articleCtlr.update_put);
+router.patch('/:id', articleCtlr.update_patch);
+router.delete('/:id', articleCtlr.delete);
 
 module.exports = router
